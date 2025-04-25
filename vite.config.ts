11d03b538227fbd819bf78ee6d375256a8381233
Rajdeep Saha <146000986+C0D3K0NG@users.noreply.github.com>
2025-04-25 16:20:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Add base URL for GitHub Pages
-  base: mode === 'production' ? '/mariana/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
